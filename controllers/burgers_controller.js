f28e6577a83615c3d7ee268a3883e048032e95f9
Jonathan Jackson <jj99999@users.noreply.github.com>
2016-11-10 13:54:30 -0500

    Update burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,9 +10,9 @@ router.get('/', function (req, res) {
 
 router.get('/burgers', function (req, res) {
 	burger.all(function (data) {
-		var hbsObject = { burgers: data };
-		console.log(hbsObject);
-		res.render('index', hbsObject);
+		var burgObject = { burgers: data };
+		console.log(burgObject);
+		res.render('index', burgObject);
 	});
 });
 
